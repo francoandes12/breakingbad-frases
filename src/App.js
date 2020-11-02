aa787b1a3239e8d0a9a8e9ba84a09dcf52f368f8
francoandes12 <francoande12@gmail.com>
2020-11-01 22:10:08 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import styled from '@emotion/styled'
+import { useState,useEffect } from 'react'
+import { Frase } from './components/Frase'
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`
+const Boton = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
   );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size 0.8s ease;
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`
+function App() {
+  const [frase, setFrase] = useState({})
+  const handleConsultarApi = async () => {
+    const api = await fetch(
+      'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
+    )
+    const frase = await api.json()
+    setFrase(frase[0])
+  }
+  useEffect(() => {
+    handleConsultarApi()
+  }, [])
+  
+  return (
+    <Contenedor>
+      <Frase frase={frase} />
+      <Boton onClick={handleConsultarApi}>Obtener Frase</Boton>
+    </Contenedor>
+  )
 }
 
-export default App;
+export default App
